7aac50fb1c280c186a87db830749c2670718ae31
Miguel <gannasong@gmail.com>
2021-02-25 17:27:30 +0800

    setup NavigatorMap url

--- AppRouterTests/AppRouterTests.swift
+++ AppRouterTests/AppRouterTests.swift
@@ -9,20 +9,37 @@ import XCTest
 import URLNavigator
 @testable import AppRouter
 
-class AppRouter {}
+class AppRouter {
+
+  func setNavigationMap() {
+    NavigatorProxy.shared.registeredURL = "https://first-test-url.com"
+  }
+}
 
 class NavigatorProxy {
+  static let shared = NavigatorProxy()
+
   let navigator = Navigator()
+
+  private init() {}
+
+  var registeredURL: String?
 }
 
 class AppRouterTests: XCTestCase {
 
   func test_init_navigatorProxyNavigatorShouldNotNil() {
-    let proxy = NavigatorProxy()
+    let proxy = NavigatorProxy.shared
     _ = AppRouter()
     XCTAssertNotNil(proxy.navigator)
   }
 
   func test_init_navigatorShouldRegisterOnePath() {
+    let proxy = NavigatorProxy.shared
+    let sut = AppRouter()
+
+    sut.setNavigationMap()
+
+    XCTAssertNotNil(proxy.registeredURL)
   }
 }
