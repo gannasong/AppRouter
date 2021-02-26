16229ebbb4c8e22537627df1f5df09111c15734e
Miguel <gannasong@gmail.com>
2021-02-26 16:21:55 +0800

    extend path and pattern cases

--- AppRouter/Router.swift
+++ AppRouter/Router.swift
@@ -12,10 +12,19 @@ public enum TestPaths: String, CaseIterable {
   case home_01 = "https://icook.tw"
   case home_02 = "https://icook.tw/"
   case amp_categories = "https://icook.tw/amp/categories/55"
+  case amp_search = "https://icook.tw/amp/search/cake/"
+  case richSearch = "https://icook.tw/search/羊肉/空心菜,辣椒/"
 
   public static var allPaths: [String] {
     TestPaths.allCases.map { $0.rawValue }
   }
+}
+
+public enum RouterPatterns: String {
+  case home = "https://icook.tw/"
+  case amp_categories = "https://icook.tw/amp/categories/<int:id>"
+  case amp_search = "https://icook.tw/amp/search/string:keyword/"
+  case richSearch = "https://icook.tw/search/<string:keyword>/<string:ingredients>"
 }
 
 public protocol NavigatorProxy {
